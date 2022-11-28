0c6c300a505d8b46e807f456e6140495af745a0f
Juun Pak <june.mintlatte@gmail.com>
2022-11-28 21:07:29 +0900

    feat: Implement a list of goals using FlatList (instead of ScrollView)

--- App.tsx
+++ App.tsx
@@ -1,20 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList,
+} from "react-native";
+
+interface Goals {
+  text: string;
+  id: string;
+}
 
 export default function App() {
+  const [enteredText, setEnteredText] = useState("");
+  const [goals, setGoals] = useState<Goals[]>([]);
+
+  const handleChangeText = (text: string) => {
+    setEnteredText(text);
+  };
+
+  const handlePress = () => {
+    setGoals((prev) => [
+      ...prev,
+      { text: enteredText, id: Math.random().toString() },
+    ]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputWrapper}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Add your goal"
+          onChangeText={handleChangeText}
+        />
+        <Button title="Add Goal" onPress={handlePress} />
+      </View>
+      <View style={styles.outputWrapper}>
+        <FlatList
+          data={goals}
+          renderItem={({ item }) => (
+            <View style={styles.goalItem}>
+              <Text style={styles.goalText}>{item.text}</Text>
+            </View>
+          )}
+          keyExtractor={({ id }) => id}
+          alwaysBounceVertical={false}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 50,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  inputWrapper: {
+    padding: 16,
+    paddingBottom: 32,
+    flexDirection: "row",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  textInput: {
+    padding: 16,
+    marginRight: 8,
+    flex: 1,
+    borderWidth: 1,
+    borderColor: "#cccccc",
+  },
+  outputWrapper: {
+    padding: 20,
+  },
+  goalItem: {
+    padding: 12,
+    marginBottom: 8,
+    backgroundColor: "#FDE40C",
+    borderRadius: 8,
+  },
+  goalText: {
+    color: "#fff",
+    fontSize: 14,
   },
 });
